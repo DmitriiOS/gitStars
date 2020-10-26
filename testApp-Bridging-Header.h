35ef1e781aff5232d38f50174f43c313ac07a7cf
Dmitriy Orlov <dmitriy.orlov@omega-r.com>
2020-10-26 16:00:23 +0300

    successful building. Refactoring in process.

---
+++ testApp-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  testApp-Bridging-Header.h
+//  testApp
+//
+//  Created by Dmitriy Orlov on 26.10.2020.
+//
+
+#ifndef testApp_Bridging_Header_h
+#define testApp_Bridging_Header_h
+
+
+#endif /* testApp_Bridging_Header_h */
